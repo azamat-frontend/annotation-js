7fcc73018111a410ccd62c4f2512e09a93f45f89
azamatfrontend <azamatfrontend@gmail.com>
2022-02-25 16:35:41 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import {annotate, annotationGroup} from "https://unpkg.com/rough-notation?module"
+
+const header = document.querySelector('.heading');
+const hashnode = document.querySelector('.hashnode');
+const block =  document.querySelector('.block');
+const line =  document.querySelector('.line');
+
+const annotation = annotate(header, { type: 'highlight', color:'skyblue' });
+const annotation2 = annotate(hashnode, { type:'circle', color:'yellow', padding:7});
+const annotation3 = annotate(line, { type: 'underline', color:'blue' });
+const annotation4 = annotate(block, {type:'bracket', color:'pink',brackets: ['left', 'right'], strokeWidth:5});
+const array = annotationGroup([annotation, annotation2, annotation3, annotation4]);
+array.show()
